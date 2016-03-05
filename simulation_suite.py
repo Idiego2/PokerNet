8e32b2b726f620b659aa736efd546fa7c56896a4
Hunter H <huntrar@gmail.com>
2016-03-05 00:38:27 -0800

    Made consistent with default args

--- simulation_suite.py
+++ simulation_suite.py
@@ -1,65 +1,73 @@
 """PokerNet simulation suite"""
 
-from train import run_simulation
+from train import run_simulation, get_parser
 
 
 def run_simulations():
     """Run ANN training simulations with various methods and parameters"""
+    # Initialize args with program defaults
+    init_args = vars(get_parser().parse_args())
 
     # Table 1: Training results for different neuron numbers
-    args = {'method': 'gdm',
+    args = init_args.copy()
+    args.update({'method': 'gdm',
             'activation': 'purelin',
             'max_epoch': 1000,
-            'learning_rate': 0.02}
+            'learning_rate': 0.02})
     for i in (10, 30, 50):
         args['hidden_neurons'] = i
         run_simulation(args)
 
 
     # Table 2: Effect of the change in learning rate value
-    args = {'method': 'gdm',
+    args = init_args.copy()
+    args.update({'method': 'gdm',
             'activation': 'purelin',
             'max_epoch': 1000,
-            'learning_rate': 0.2}
+            'learning_rate': 0.2})
     for i in (10, 30, 50):
         args['hidden_neurons'] = i
         run_simulation(args)
 
 
     # Table 3: Effect of different epoch limits for 10 neurons
-    args = {'method': 'gdm',
+    args = init_args.copy()
+    args.update({'method': 'gdm',
             'activation': 'purelin',
             'learning_rate': 0.2,
-            'hidden_neurons': 10}
+            'hidden_neurons': 10})
     for i in (1000, 2000, 3000):
         args['epochs'] = i
         run_simulation(args)
 
 
     # Table 4: Effect of different epoch limits for 30 neurons
-    args = {'method': 'gdm',
+    args = init_args.copy()
+    args.update({'method': 'gdm',
             'activation': 'purelin',
             'learning_rate': 0.2,
-            'hidden_neurons': 30}
+            'hidden_neurons': 30})
     for i in (1000, 2000, 3000):
         args['epochs'] = i
         run_simulation(args)
 
 
     # Table 5: Effect of different epoch limits for 50 neurons
-    args = {'method': 'gdm',
+    args = init_args.copy()
+    args.update({'method': 'gdm',
             'activation': 'purelin',
             'learning_rate': 0.2,
-            'hidden_neurons': 50}
+            'hidden_neurons': 50})
     for i in (1000, 2000, 3000):
         args['epochs'] = i
         run_simulation(args)
 
 
     # Table 6: comparison of three methods for 10 neurons
-    args = {'activation': 'purelin',
+    args = init_args.copy()
+    args.update({'activation': 'purelin',
             'max_epoch': 1000,
-            'hidden_neurons': 10}
+            'hidden_neurons': 10})
     for mthd in ('gdm', 'scg', 'rp'):
         args['method'] = mthd
 
@@ -69,9 +77,10 @@ def run_simulations():
 
 
     # Table 7: Effect of transfer function for resilient back-propagation method for 1000 iteration
-    args = {'method': 'rp',
+    args = init_args.copy()
+    args.update({'method': 'rp',
             'max_epoch': 1000,
-            'hidden_neurons': 10}
+            'hidden_neurons': 10})
     for n_num in (30, 50):
         args['hidden_neurons'] = n_num
 
@@ -84,9 +93,10 @@ def run_simulations():
 
 
     # Table 8: Effect of transfer function for resilient back-propagation method for 2000 iteration
-    args = {'method': 'rp',
+    args = init_args.copy()
+    args.update({'method': 'rp',
             'max_epoch': 2000,
-            'hidden_neurons': 10}
+            'hidden_neurons': 10})
     for n_num in (10, 30, 50):
         args['hidden_neurons'] = n_num
 
@@ -99,9 +109,10 @@ def run_simulations():
 
 
     # Table 9: Effect of transfer function for scaled conjugate gradient method
-    args = {'method': 'scg',
+    args = init_args.copy()
+    args.update({'method': 'scg',
             'max_epoch': 1000,
-            'hidden_neurons': 10}
+            'hidden_neurons': 10})
     for act_fn in ('purelin', 'tansig'):
         args['activation'] = act_fn
 
@@ -112,8 +123,9 @@ def run_simulations():
 
     # Table 10: Effect of transfer function for scaled conjugate gradient method
     # Vary validation limit (what is that?)
-    args = {'method': 'scg',
-            'hidden_neurons': 50}
+    args = init_args.copy()
+    args.update({'method': 'scg',
+            'hidden_neurons': 50})
     validation_limit = 100
     # set validation limit to 100 here
     args['max_epoch'] = 1000
@@ -132,6 +144,7 @@ def run_simulations():
         run_simulation(args)
 
     # Table 11: Test results for various networks
+    args = init_args.copy()
 
 
 
